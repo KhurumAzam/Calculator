a95ab2ae8efdee42937bdc70afaeef7f3fb026e0
KhurumAzam <76566157+KhurumAzam@users.noreply.github.com>
2023-02-23 07:30:26 -0500

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,4 +1,8 @@
 class Calculator:
+    def __init__(self, num1, num2):
+        self.num1 = num1
+        self.num2 = num2
+    
     def add(self, num1, num2): return num1 + num2
     def sub(self, num1, num2): return num1 - num2
     def multi(self, num1, num2): return num1 * num2
@@ -6,24 +10,3 @@ class Calculator:
     def pow(self, num1, num2): return num1 ** num2
     def mod(self, num1, num2): return num1 % num2
     def flo(self, num1, num2): return num1 // num2
-
-    def print_table(self, operator):
-        for i in range(999):
-            for j in range(999):
-                try:
-                    if operator == "Addition":
-                        print(i, " + ", j, " = ", self.add(i,j))
-                    elif operator == "Subtraction":
-                        print(i, " - ", j, " = ", self.sub(i,j))
-                    elif operator == "Multiplication":
-                        print(i, " * ", j, " = ", self.multi(i,j))
-                    elif operator == "Division":
-                        print(i, " / ", j, " = ", self.div(i,j))
-                    elif operator == "Power":
-                        print(i, " ** ", j, " = ", self.pow(i,j))
-                    elif operator == "Modulus":
-                        print(i, " % ", j, " = ", self.mod(i,j))
-                    elif operator == "Float":
-                        print(i, " // ", j, " = ", self.flo(i,j))
-                except:
-                    print ("An error occurred")
